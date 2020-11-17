244c0107a909d60d4bb1fd75bdcd7b66ee71c2d6
kbc18a11 <wada121400@gmail.com>
2020-11-17 14:17:29 +0900

    ファイル名変更

---
+++ 02/kadai.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://paiza.jp/student/search?c%5Bnew_graduates_target_year%5D=2022&recommend=true"
+
+res = requests.get(url)
+
+soup = BeautifulSoup(res.content, "html.parser")
+
+# print(soup.find(id="chap2"))
+
+offerBox = soup.find(class_="boxPickup")
+
+companyInfo = [[], []]
+
+for element in offerBox.find_all(class_="c-job_offer-box"):
+    print(element.find(class_="c-job_offer-recruiter__name").text)
+    print(element.find(class_="c-job_offer-detail__occupation").text)
+
+    try:
+        print(element.find(class_="c-job_offer-detail__salary").text)
+    except AttributeError:
+        print("給料が存在しません")
+
+    print("\n")
